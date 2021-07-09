a25b94d5e1ba7e1227e53f700a283c5ee11d8d3e
Qutred <shloser.andriy@gmail.com>
2021-07-09 10:05:59 +0300

    added dynamic pizza load, piiza comp, pizza db

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 import './scss/app.scss';
 
 import { Header } from './components/';
 import { Home, Cart } from './pages';
 
 function App() {
+	const [pizzas, SetPizzas] = React.useState([]);
+
+	React.useEffect(() => {
+		axios.get('http://localhost:3000/db.json').then(({ data }) => {
+			SetPizzas(data.pizzas);
+		});
+	}, []);
+
 	return (
 		<div className='wrapper'>
 			<Header />
 			<div className='content'>
 				<Switch>
-					<Route exact path='/' component={Home} />
+					<Route
+						exact
+						path='/'
+						render={() => {
+							return <Home items={pizzas} />;
+						}}
+					/>
 					<Route path='/cart' component={Cart} />
 				</Switch>
 			</div>
